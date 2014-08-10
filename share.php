bc816c64729b153812ddb9915c55abd4cc843356
raish <shomarai@pdx.edu>
2014-08-10 15:39:16 -0700

    Added php file - to connect to social media

---
+++ share.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<script type="text/javascript" src="http://www.skypeassets.com/i/scom/js/skype-uri.js"></script>
+<div id="SkypeButton_Call_rai.shoma_1">
+  <script type="text/javascript">
+    Skype.ui({
+      "name": "dropdown",
+      "element": "SkypeButton_Call_rai.shoma_1",
+      "participants": ["rai.shoma"],
+      "imageSize": 32,
+    });
+  </script>
+</div>
+</head>
+<body background='redwood.jpg'>
+<a href="https://www.facebook.com/" target="_blank" align="left"><img src="http://www.niftybuttons.com/webicons2/facebook.png" border="0" style="margin:1px;"></a>
+<a href="https://twitter.com/" target="_blank" align="left"><img src="http://www.niftybuttons.com/webicons2/twitter.png" border="0" style="margin:1px;"></a>
+<a href="https://www.youtube.com/" target="_blank" align="left"><img src="http://www.niftybuttons.com/webicons2/youtube.png" border="0" style="margin:1px;"></a>
+<a target="_blank" href="https://plus.google.com "><IMG src="http://www.topproducerwebsite.com/images/site_defaults/generic/Googleplus.png"></a>
+<a target="_blank" href="http://twitter.com/"><IMG src="http://www.topproducerwebsite.com/images/site_defaults/generic/twitter.png"></a>
+<a target="_blank" href="http://myspace.com/username"><IMG src="http://www.topproducerwebsite.com/images/site_defaults/generic/myspace.png"></a>
+</body>
+</html>
